f2378e02aa86d640fa72bf3494fd3b3dae29320f
xilin xia <xiaxilin1989@gmail.com>
2020-05-17 20:34:45 +0100

    Update manning_only.m

--- manning_only.m
+++ manning_only.m
@@ -38,32 +38,6 @@ N = 6000;
 %
 
 for i = 1:N
-    % first step of RK2
-%     q_b =  hu(i)*hC(i)/h(i);
-%     u = hu(i)/h(i);
-%     C = hC(i)/h(i);
-%     rho = rho_s*(1-C) + rho_w*C;
-%     q_b_star = A_g*u^3;
-%     dhdt = -1.0*(q_b - q_b_star)/L/(1-p);
-%     dhcdt = -1.0*(q_b - q_b_star)/L;
-%     S_f = n^2*h(i)^(-4/3)*u^2;
-%     dhudt = g*h(i)*(S_b-S_f); % - (rho_s - rho_w)/rho*u*dhdt*(1-p-C);
-%     hu_1 = hu(i); % + dt*dhudt;
-%     h_1 = h(i); % + dt*dhdt;
-%     hC_1 = hC(i) + dt*dhcdt;
-%     % Second step of RK2
-%     q_b =  hu_1*hC_1/h_1;
-%     u = hu_1/h_1;
-%     C = hC_1/h_1;
-%     rho = rho_s*(1-C) + rho_w*C;
-%     q_b_star = A_g*u^3;
-%     dhdt_1 = -1.0*(q_b - q_b_star)/L/(1-p);
-%     dhcdt_1 = -1.0*(q_b - q_b_star)/L;
-%     S_f = n^2*h_1^(-4/3)*u^2;
-%     dhudt_1 = g*h_1*(S_b-S_f); % - (rho_s - rho_w)/rho*u*dhdt_1*(1-p-C);
-%     hu(i+1) = hu(i); % + 0.5*dt*(dhudt + dhudt_1);
-%     h(i+1) = h(i); % + 0.5*dt*(dhdt + dhdt_1);
-%     hC(i+1) = hC(i) + 0.5*dt*(dhcdt + dhcdt_1);
 % Fully implicit scheme for sediment transport
    q_b =  hu(i)*hC(i)/h(i);
    u = hu(i)/h(i);
@@ -71,10 +45,6 @@ for i = 1:N
    U_0 = [hC(i) h(i) u]';
    U_1 = [hC(i) h(i) u]';
    for j = 1:10
-    %J = [-U_1(3)/L 0 -U_1(1)/L+3*A_g*U_1(3)^2/L;-U_1(3)/L/(1-p) 0 -U_1(1)/L/(1-p)+3*A_g*U_1(3)^2/L/(1-p);0 -g*n^2*U_1(3)^2*(-4/3)*U_1(2)^(-7/3) -g*n^2*2*U_1(3)*U_1(2)^(-4/3)];
-    %J = myfunc_Jac(A_g, L, g, n, p, U_1(1), U_1(2), U_1(3), rho_s, rho_w);
-%     J = myfunc_Jac(A_g, L, g, n, p, U_1(1), U_1(2), U_1(3));
-    %J = myfunc_Jac(g, n, U_1(2), U_1(3));
     J = myfunc_Jac(A_g, L, g, n, p, U_1(1), U_1(2), U_1(3), rho_s, rho_w);
     q_b_star = A_g*U_1(3)^3;
     S_1 = -(U_1(3)*U_1(1)-q_b_star)/L;
